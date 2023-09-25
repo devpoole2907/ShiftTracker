38a963ff3101e9eff916036ef687ee4bdc9715da
James Poole <jamesdpoole@icloud.com>
2023-09-25 18:21:51 +1300

    Animated mockup lockscreen view

--- ShiftTracker/Intro/MockupLockscreenView.swift
+++ ShiftTracker/Intro/MockupLockscreenView.swift
@@ -8,6 +8,9 @@
 import SwiftUI
 
 struct MockupLockscreenView: View {
+    
+    @State private var switchView: Bool = true
+    
     var body: some View {
      
             VStack(spacing: 0) {
@@ -15,8 +18,11 @@ struct MockupLockscreenView: View {
                 VStack(spacing: 0){
                     HStack {
                         RoundedRectangle(cornerRadius: 4).frame(width: 60, height: 10)
-                        
-                        Text("$42.65").bold().font(.subheadline)
+                        if switchView {
+                            Text("$42.65").bold().font(.subheadline)
+                        } else {
+                            Text("No current shift").bold().font(.subheadline)
+                        }
                         
                     }
                     Text("9:41").bold()
@@ -27,22 +33,50 @@ struct MockupLockscreenView: View {
                     
                 }
                 HStack(spacing: 20) {
-                    VStack(alignment: .leading){
-                        Text("Shift Duration:")
-                        Text(Date(), style: .timer)
-                    } .font(.caption)
-                        .bold()
-                        .padding(5).background(.ultraThinMaterial).cornerRadius(5)
                     
-                    Text("No current shift")
-                        .font(.caption)
-                        .bold()
-                        .padding(5).background(.ultraThinMaterial).cornerRadius(5)
+                    if switchView {
+                        
+                        VStack(alignment: .leading){
+                            Text("Shift Duration:")
+                            Text(Date().addingTimeInterval(-400), style: .timer)
+                        } .font(.caption)
+                            .bold()
+                            .padding(5).background(.ultraThinMaterial).cornerRadius(5)
+                        
+                        VStack(alignment: .leading){
+                            Text("Current Pay:")
+                            Text("$42.65")
+                        } .font(.caption)
+                            .bold()
+                            .padding(5).background(.ultraThinMaterial).cornerRadius(5)
+                        
+                    } else {
+                        
+                        
+                        Text("No current shift")
+                            .font(.caption)
+                            .bold()
+                            .padding(5).background(.ultraThinMaterial).cornerRadius(5)
+                        
+                        
+                        Text("No current shift")
+                            .font(.caption)
+                            .bold()
+                            .padding(5).background(.ultraThinMaterial).cornerRadius(5)
+                        
+                    }
                     
                 }
                 
             }     .foregroundStyle(.gray)
             
+            .onAppear {
+                Timer.scheduledTimer(withTimeInterval: 4, repeats: true) { _ in
+                            withAnimation {
+                                switchView.toggle()
+                            }
+                        }
+                    }
         
         
     }
