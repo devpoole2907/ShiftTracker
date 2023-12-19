3e97df373694f0d9b6ffb90fb5b968ab4dc3511e
James Poole <131243847+devpoole2907@users.noreply.github.com>
2023-12-20 07:33:53 +1300

    Update CustomEditButton.swift
    Optionally accepts a title

--- ShiftTracker/Views/CustomUIElements/CustomEditButton.swift
+++ ShiftTracker/Views/CustomUIElements/CustomEditButton.swift
@@ -13,17 +13,31 @@ import SwiftUI
 struct CustomEditButton: View {
     @Binding var editMode: EditMode
     var action: () -> Void
+    var title: String? 
     
+    init(editMode: Binding<EditMode>, action: @escaping () -> Void, title: String? = nil) {
+        self._editMode = editMode
+        self.action = action
+        self.title = title
+    }
+
     var body: some View {
         Button(action: {
             withAnimation {
                 editMode = (editMode == .active) ? .inactive : .active
-                
-                
                 action()
             }
         }) {
-            Text(editMode == .active ? "Done" : "Edit")
+            
+                if let buttonTitle = title {
+                    HStack {
+                        Image(systemName: "ellipsis.circle")
+                        Text(buttonTitle)
+                     }
+                } else {
+                
+                Text(editMode == .active ? "Done" : "Edit")
+            }
         }
     }
 }
